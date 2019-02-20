3eb90b28d930139ed3d2426bc83867468ed3a02d
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-20 13:12:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var product1 = 'Dell Inspiron 3565'
 var product2 = 'ThinkPad L380'
 var text1 = 'You have' + number1  + 'orders in your order list, and the details are' + product1 + 'it will be delivered at your shipping address in 5 days.'
 
-var text2 = 'ddddd' + product2  +  'be delivered deliver at your shipping address in 3 days'
+var text2 = '' + product2  +  'be delivered deliver at your shipping address in 3 days'
 
 //var text3 = 'We have fantastic deals available on eBook reader would you like to check it?'
 
