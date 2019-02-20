c14eee22268ce24d669cc6ea27d1ab21c664956f
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-20 12:13:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.post('/webhook', function (req, res) {
   var userName = 'sandeep'
   var number1 = '2'
   //var number2 = '5'
-  //var product1 = 'Dell Inspiron 3565'
+  var product1 = 'Dell Inspiron 3565'
   //var product2 = 'ThinkPad L380' 
-  var webhookReply = 'You have ' + number1 + '! orders in your order list, and the details are.'
+  var webhookReply = 'You have ' + number1 + '! orders in your order list, and the details are.' + product1 + 'it will be delivered at your shipping address in'.
   
   
 //var webhookReply1 = 'You have' +  number1 + 'orders in your order list, and the details are' : + 
