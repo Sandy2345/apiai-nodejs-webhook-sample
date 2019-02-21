c0d09346ad688ec02288bed3f158cd49af5afff9
Sandy2345 <43490410+Sandy2345@users.noreply.github.com>
2019-02-21 11:50:23 +0530

    Update indexxx.js

--- indexxx.js
+++ indexxx.js
@@ -1,5 +1,5 @@
 var OmnitureAPI = require('node-omniture-api')
-var omniture = new OmnitureAPI('<user-credentials>', '<token>');
+var omniture = new OmnitureAPI('payal.daryani@capgemini.com:Capgeminisandbox', 'e5eccca081d2a1a329ee56e41e451811');
 var pageViews;
 var duration;
 var dateFrom = new Date();
@@ -10,13 +10,10 @@ function updatePageViews(requestData,context,duration) {
 		if (success) {
 
 			pageViews = data.report.totals[0];
-			console.log(data.report.totals[0]);
-			console.log(pageViews);
-			context.succeed(generateResponse(buildSpeechletResponse("we have " + pageViews + " page views " + duration, false)))
+			
 		} else {
 			pageViews = data;
-			console.error(data);
-			context.succeed(generateResponse(buildSpeechletResponse("Sorry, Adobe Analytics experienced an error. Please try again later." + pageViews, false)))
+			
 		}
 	});
 }
@@ -45,74 +42,3 @@ function updateFromDate(duration) {
 	}
 }
 
-exports.handler = (event, context, callback) => {
-	
-	switch (event.request.type) {
-		case "LaunchRequest":
-			context.succeed(generateResponse(buildSpeechletResponse("Welcome to Adobe Analytics..How can I help you?.", false)))
-			break;
-		case "IntentRequest":
-			switch (event.request.intent.name) {
-				case "OneshotReportIntent":
-					duration = event.request.intent.slots.Duration.value;
-					updateFromDate(duration);
-					var requestData = {
-						"reportDescription": {
-
-							"reportSuiteID": "geo1xxlon-we-retail-demo",
-							"dateFrom": dateFrom.toISOString().slice(0, 10),
-							"dateTo": dateTo.toISOString().slice(0, 10),
-							"metrics": "[{ id: 'pageviews' }]"
-
-						}
-					}
-					updatePageViews(requestData,context,duration);
-					
-					break;
-				case "ReportSuiteSelectionIntent":
-					
-					var reportSuites = event.request.intent.slots.ReportSuite.value;
-					context.succeed(generateResponse(buildSpeechletResponse("Hello World chirag 2 " + reportSuites, false)))
-					break;
-				case "PageViewsTodayIntent":
-					duration = "today";
-					requestData = {
-						"reportDescription": {
-							"source": "realtime",
-							"reportSuiteID": "geo1xxlon-we-retail-demo",
-
-							"metrics": "[{ id: 'pageviews' }]"
-
-						}
-					}
-					updatePageViews(requestData,context,duration);
-					
-					break;
-				case "ThankYouIntent":
-
-					context.succeed(generateResponse(buildSpeechletResponse("My pleasure, have a fantastic day! ", true)))
-					break;
-				case "AMAZON.StopIntent":
-					context.succeed(generateResponse(buildSpeechletResponse("Goodbye!", true)))
-					break;
-			}
-			break;
-	}
-}
-
-
-var buildSpeechletResponse = (outputText, shouldEndSession) => {
-	return {
-		outputSpeech: {
-			type: "PlainText",
-			text: outputText
-		},
-		shouldEndSession: shouldEndSession
-	}
-}
-var generateResponse = (speechletResponse) => {
-	return {
-		version: "1.0",
-		response: speechletResponse
-	}
-}
